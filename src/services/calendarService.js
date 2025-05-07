1abc903e16aa523a8a28cbb149688d95e0f7cea1
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-07 11:02:31 +0000

    hardcode timezone

--- src/services/calendarService.js
+++ src/services/calendarService.js
@@ -41,27 +41,27 @@ exports.createEvent = async (accessToken, eventDetails) => {
   const startDateTime = new Date(eventDetails.meetingStartTime);
   const endDateTime = new Date(startDateTime.getTime() + 30 * 60 * 1000); // Add 30 minutes
 
-console.log(startDateTime, startDateTime.toISOString(), endDateTime, eventDetails.timeZone);
-console.log(typeof startDateTime, typeof endDateTime, typeof eventDetails.timeZone, typeof eventDetails.email);
+  console.log(startDateTime, startDateTime.toISOString(), endDateTime, eventDetails.timeZone);
+  console.log(typeof startDateTime, typeof endDateTime, typeof eventDetails.timeZone, typeof eventDetails.email);
   const event = {
     'summary': `WeBuild introduction call with ${eventDetails.name}`,
     'description': `${eventDetails.extraDetails}`,
     'start': {
       'dateTime': startDateTime.toISOString(),
-      'timeZone': `${eventDetails.timeZone}`,
+      'timeZone': 'Europe/London',
     },
     'end': {
       'dateTime': endDateTime.toISOString(),
-      'timeZone': `${eventDetails.timeZone}`,
+      'timeZone': 'Europe/London',
     },
     'attendees': [
-      {'email': `${eventDetails.email}`},
+      { 'email': `${eventDetails.email}` },
     ],
     'reminders': {
       'useDefault': false,
       'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 60},
+        { 'method': 'email', 'minutes': 24 * 60 },
+        { 'method': 'popup', 'minutes': 60 },
       ],
     },
     conferenceData: {
@@ -81,7 +81,7 @@ console.log(typeof startDateTime, typeof endDateTime, typeof eventDetails.timeZo
       conferenceDataVersion: 1 // Required to enable Google Meet
     });
 
-    return response.data; 
+    return response.data;
   } catch (error) {
     console.error('Error creating calendar event:', error);
     throw new Error('Failed to create calendar event');
